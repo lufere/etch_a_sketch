2ff68b02bf8e45daca047d76f4c198f7b9f52e5a
Luis Rendon <luisfelipe.rendon@gmail.com>
2020-03-01 21:42:01 -0700

    Added the createGrid function which creates a set number of divs based on the grid size and sets the rows and columns based on the same number

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('#container');
+
+function createGrid(gridSize){
+    for(let i=0;i<gridSize;i++){
+      for(let j=0;j<gridSize;j++){
+        this['div'+j+'_'+i] = document.createElement('div');
+        this['div'+j+'_'+i].classList.add('tile');
+        //this['div'+j+'_'+i].textContent=i+"-"+j;
+        container.appendChild(this['div'+j+'_'+i]);
+      }
+    }
+    container.style.setProperty('grid-template-columns', `repeat(${gridSize}, 1fr)`);
+    container.style.setProperty('grid-template-rows', `repeat(${gridSize}, 1fr)`);
+  }
+
+  createGrid(50);
